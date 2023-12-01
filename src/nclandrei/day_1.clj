3bd702236092284f7b016cb0f7ceffea48eb8653
Andrei-Mihai Nicolae <contact@anicolae.ro>
2023-12-01 23:31:43 +0200

    work on day 1 part 2

--- src/nclandrei/day_1.clj
+++ src/nclandrei/day_1.clj
@@ -1,23 +1,68 @@
 (ns day-1
-  (:require [clojure.java.io :as io])
-  )
+  (:require [clojure.java.io :as io]))
 
-(def digits [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9])
+(def letter-digits
+  {"one" 1
+   "two" 2
+   "three" 3
+   "four" 4
+   "five" 5
+   "six" 6
+   "seven" 7
+   "eight" 8
+   "nine" 9})
+
+(def pattern #"(?i)(\d|one|two|three|four|five|six|seven|eight|nine)")
 
 (defn read-data [fname]
   (with-open [rdr (io/reader fname)]
     (doall (line-seq rdr))))
 
-(defn number
+(defn digit
+  [d]
+  (if (contains? letter-digits d) (letter-digits d) d))
+
+(defn number-1
   [line]
   (let [dd (filter #(Character/isDigit %) line)]
     (Integer/parseInt (str (first dd) (last dd)))))
+
+(defn number-2
+  [line]
+  (let [matches (re-seq pattern line)
+        first-match (last (first matches))
+        last-match (last (last matches))]
+    (Integer/parseInt (str (digit first-match) (digit last-match)))))
 
 (defn day-1-part-1
   []
   (->>
    (read-data "/Users/anicolae/Code/advent-of-code-2023/resources/day-1.txt")
-   (map number)
+   (map number-1)
    (apply +)))
 
-(day-1-part-1)
+(defn day-1-part-2
+  []
+  (->>
+   (read-data "/Users/anicolae/Code/advent-of-code-2023/resources/day-1.txt")
+   (map number-2)
+   (apply +)))
+
+(day-1-part-2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
