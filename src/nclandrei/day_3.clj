45b26602f34c656c66afc6703d4b7e01fef0f330
Andrei-Mihai Nicolae <contact@anicolae.ro>
2023-12-11 23:03:00 +0200

    work on day 3 part 2

--- src/nclandrei/day_3.clj
+++ src/nclandrei/day_3.clj
@@ -7,7 +7,7 @@
   (let [m (re-matcher pattern line)]
     ((fn step []
        (when (. m find)
-         (cons {:start (. m start) :y y :end (. m end) :group (Integer/parseInt (. m group))}
+         (cons {:start (. m start) :y y :end (. m end) :group (. m group)}
                (lazy-seq (step))))))))
 
 (defn chars-on-line
@@ -18,25 +18,44 @@
   [y]
   (or (get data y) []))
 
+(defn positions
+  [y start]
+  (if (nil? (get data y)) [] ()))
+
 (defn adjacent-chars
   [y start end]
-   (map #(chars-on-line % start end)
-        [(line y)
-         (line (inc y))
-         (line (dec y))]))
+  (map #(chars-on-line % start end)
+       [(line y)
+        (line (inc y))
+        (line (dec y))]))
+
+(defn adjacent-star-positions
+  [y start]
+  [])
 
 (defn part-number?
   [y start end]
   (let [adjacent-chars (flatten (adjacent-chars y start end))]
-      (not (empty? (filter #(and (not (Character/isDigit %)) (not= % \.)) adjacent-chars)))))
+    (not (empty? (filter #(and (not (Character/isDigit %)) (not= % \.)) adjacent-chars)))))
 
-(defn day-1-part-1
+(defn day-3-part-1
   []
   (->>
    (map-indexed (fn [idx line] (re-seq-pos #"\d+" line idx)) data)
    (flatten)
    (filter #(and (some? %) (part-number? (% :y) (% :start) (% :end))))
-   (map #(% :group))
+   (map #(Integer/parseInt (% :group)))
    (apply +)))
 
-(day-1-part-1)
+(defn day-3-part-2
+  []
+  (let [numbers (map-indexed (fn [idx line] (re-seq-pos #"\d+" line idx)) data)]
+    (->>
+     (map-indexed (fn [idx line] (re-seq-pos #"\*" line idx)) data)
+     (flatten))))
+
+(day-3-part-1)
+
+(day-3-part-2)
+
+
