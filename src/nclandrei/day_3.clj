922b98d3b4a192cb70710b8398eb45b977ea05ea
Andrei-Mihai Nicolae <contact@anicolae.ro>
2023-12-12 10:44:21 +0200

    complete day 3 part 2

--- src/nclandrei/day_3.clj
+++ src/nclandrei/day_3.clj
@@ -30,8 +30,12 @@
         (line (dec y))]))
 
 (defn adjacent-star-positions
-  [y start]
-  [])
+  [x y]
+  (->>
+   (apply concat [(if (= y 0) [] (partition 2 (interleave [(dec x) x (inc x)] (repeat 3 (dec y)))))
+                  [[(dec x) y] [(inc x) y]]
+                  (if (= y (dec (count data))) [] (partition 2 (interleave [(dec x) x (inc x)] (repeat 3 (inc y)))))])
+   (filter (fn [[x y]] (and (and (>= x 0) (< x (count (first data)))) (and (>= y 0) (< y (count data))))))))
 
 (defn part-number?
   [y start end]
@@ -47,15 +51,63 @@
    (map #(Integer/parseInt (% :group)))
    (apply +)))
 
+(defn gear-numbers
+  [item numbers]
+  (let [{star-y-pos :y star-x-pos :start} item
+        adjacent-star-positions (adjacent-star-positions star-x-pos star-y-pos)
+        y-positions (map (fn [[x y]] y) adjacent-star-positions)
+        x-positions (map (fn [[x y]] x) adjacent-star-positions)]
+    (assoc item :numbers (filter
+                          (fn [{:keys [start end y group]}]
+                            (and
+                             (some? (some #{y} y-positions))
+                             (some? (some #(some #{%} x-positions) (range start end)))))
+                          numbers))))
+
 (defn day-3-part-2
   []
-  (let [numbers (map-indexed (fn [idx line] (re-seq-pos #"\d+" line idx)) data)]
+  (let [numbers (filter some? (flatten (map-indexed (fn [idx line] (re-seq-pos #"\d+" line idx)) data)))]
     (->>
      (map-indexed (fn [idx line] (re-seq-pos #"\*" line idx)) data)
-     (flatten))))
-
-(day-3-part-1)
+     (flatten)
+     (filter some?)
+     (map #(gear-numbers % numbers))
+     (filter #(= 2 (count (% :numbers))))
+     (map (fn [{numbers :numbers}]
+            (map #(Integer/parseInt (% :group)) numbers)))
+     (map #(apply * %))
+     (apply +))))
 
 (day-3-part-2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
