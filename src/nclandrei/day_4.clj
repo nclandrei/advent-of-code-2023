8c0a1e68530a622291720e80fb8090ef45f01a93
Andrei-Mihai Nicolae <contact@anicolae.ro>
2023-12-15 00:28:38 +0200

    work on day 4 part 2

--- src/nclandrei/day_4.clj
+++ src/nclandrei/day_4.clj
@@ -5,15 +5,6 @@
 
 (def data (vec (u/read-data "day-4.txt")))
 
-(defn number-pairs
-  []
-  (map (fn [line]
-         (->>
-          (re-seq #"Card\s+\d+: (.*) \| (.*)" line)
-          (first)
-          (rest)))
-       data))
-
 (defn numbers
   [n]
   (->>
@@ -21,14 +12,68 @@
    (map #(second %))
    (into #{})))
 
+(defn number-pairs
+  []
+  (map (fn [line]
+         (->>
+          (re-seq #"Card\s+\d+: (.*) \| (.*)" line)
+          (first)
+          (rest)
+          (map numbers)))
+       data))
+
+(defn winners
+  []
+  (map
+   (fn [[winners choices]]
+     (set/intersection (into #{} winners) (into #{} choices)))
+   (number-pairs)))
+
+(defn increment-scores
+  [acc idx count matches]
+  (reduce
+   (fn [nacc nidx] (do
+                     (println "nacc" nacc)
+                     (update nacc nidx (partial * count))))
+   acc
+   (range (inc idx) (dec matches))))
+
+(defn scratchcards
+  []
+  (let [winners (map count (winners))
+        indexed-winners (into {} (map-indexed (fn [idx itm] [idx 1]) winners))]
+    (reduce (fn [acc [idx count]]
+              (let [matches (nth winners idx)]
+                (increment-scores acc idx count matches)))
+            indexed-winners
+            indexed-winners)))
+
+(increment-scores (into {} (map-indexed (fn [idx itm] [idx 1]) (map count (winners)))) 0 1)
+
+(scratchcardsq)
+
 (defn day-4-part-1
   []
   (->>
-   (map (fn [pair] (map numbers pair)) (number-pairs))
-   (map (fn [[winners choices]] (set/intersection (into #{} winners) (into #{} choices))))
+   (winners)
    (map count)
    (map #(if (<= % 1) % (math/pow 2 (dec %))))
    (apply +)))
+
+(day-4-part-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
