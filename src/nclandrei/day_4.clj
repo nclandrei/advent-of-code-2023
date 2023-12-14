a6e120831c0e694b71e9c00cdea24bc1c4b0e6cb
Andrei-Mihai Nicolae <contact@anicolae.ro>
2023-12-14 22:57:42 +0200

    complete day 4 part 1

--- src/nclandrei/day_4.clj
+++ src/nclandrei/day_4.clj
@@ -1,4 +1,44 @@
 (ns day-4
-  (:require [util :as u]))
+  (:require [util :as u]
+            [clojure.set :as set]
+            [clojure.math :as math]))
 
 (def data (vec (u/read-data "day-4.txt")))
+
+(defn number-pairs
+  []
+  (map (fn [line]
+         (->>
+          (re-seq #"Card\s+\d+: (.*) \| (.*)" line)
+          (first)
+          (rest)))
+       data))
+
+(defn numbers
+  [n]
+  (->>
+   (re-seq #"(\d+)" n)
+   (map #(second %))
+   (into #{})))
+
+(defn day-4-part-1
+  []
+  (->>
+   (map (fn [pair] (map numbers pair)) (number-pairs))
+   (map (fn [[winners choices]] (set/intersection (into #{} winners) (into #{} choices))))
+   (map count)
+   (map #(if (<= % 1) % (math/pow 2 (dec %))))
+   (apply +)))
+
+
+
+
+
+
+
+
+
+
+
+
+
